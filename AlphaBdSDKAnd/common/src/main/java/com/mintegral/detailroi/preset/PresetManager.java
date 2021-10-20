dfbc4a8780c758e47efe71dc5a1c7fded02312c1
arige <1933095297@qq.com>
2021-10-20 15:04:09 +0800

    【修改】修改初始化时机延迟导致的开始和结束无法上报的问题

--- AlphaBdSDKAnd/common/src/main/java/com/mintegral/detailroi/preset/PresetManager.java
+++ AlphaBdSDKAnd/common/src/main/java/com/mintegral/detailroi/preset/PresetManager.java
@@ -18,12 +18,14 @@ import com.mintegral.detailroi.report.ReportManager;
 
 public class PresetManager implements Application.ActivityLifecycleCallbacks {
 
-    private  String tag = getClass().getSimpleName();
+    private String tag = getClass().getSimpleName();
     private Application mApplication;
     private long inBackgroundTime = 0l;
     private int foreGroundAcNum = 0;
     private long processStartTime;
-    private PresetManager(){}
+
+    private PresetManager() {
+    }
 
     @Override
     public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
@@ -38,22 +40,25 @@ public class PresetManager implements Application.ActivityLifecycleCallbacks {
     @Override
     public void onActivityResumed(@NonNull Activity activity) {
         updateAcInfo(activity);
-        if(foreGroundAcNum==0){
+        if (foreGroundAcNum <= 0) {
+            foreGroundAcNum = 0;
             checkBackgroundTime();
         }
         foreGroundAcNum++;
     }
-    private void updateAcInfo(Activity activity){
+
+    private void updateAcInfo(Activity activity) {
         GlobalObject.refererAcTitle = GlobalObject.currentAcTitle;
         GlobalObject.refererAcName = GlobalObject.currentAcName;
-        GlobalObject.currentAcName =activity.getClass().getName();
+        GlobalObject.currentAcName = activity.getClass().getName();
         GlobalObject.currentAcTitle = activity.getTitle().toString();
     }
 
     @Override
     public void onActivityPaused(@NonNull Activity activity) {
         foreGroundAcNum--;
-        if(foreGroundAcNum==0){
+        if (foreGroundAcNum <= 0) {
+            foreGroundAcNum = 0;
             inBackgroundTime = System.currentTimeMillis();
         }
     }
@@ -74,7 +79,7 @@ public class PresetManager implements Application.ActivityLifecycleCallbacks {
     }
 
 
-    private static class Holder{
+    private static class Holder {
         private static PresetManager presetManager = new PresetManager();
     }
 
@@ -82,62 +87,63 @@ public class PresetManager implements Application.ActivityLifecycleCallbacks {
         return Holder.presetManager;
     }
 
-    public void initPresetModule(Application application){
+    public void initPresetModule(Application application) {
         mApplication = application;
         init();
     }
 
-    private void init(){
+    private void init() {
         SessionIdsManager.createSessionId();
         mApplication.registerActivityLifecycleCallbacks(this);
-        SameLogTool.d(tag,"preset module init ok");
+        SameLogTool.d(tag, "preset module init ok");
         checkInstallState();
     }
 
 
-    private void checkInstallState(){
-        String s = (String) SharedPreferencesUtils.getParam(GlobalObject.application,"alpha_ins_label","");
-        if(TextUtils.isEmpty(s)){
-            SharedPreferencesUtils.setParam(GlobalObject.application,"alpha_ins_label","ok");
+    private void checkInstallState() {
+        String s = (String) SharedPreferencesUtils.getParam(GlobalObject.application, "alpha_ins_label", "");
+        if (TextUtils.isEmpty(s)) {
+            SharedPreferencesUtils.setParam(GlobalObject.application, "alpha_ins_label", "ok");
             reportInstallEvent();
         }
     }
 
 
-    private void checkBackgroundTime(){
+    private void checkBackgroundTime() {
         long currentTime = System.currentTimeMillis();
-        if(inBackgroundTime != 0 && currentTime-inBackgroundTime >30 *1000){
+        if (inBackgroundTime != 0 && currentTime - inBackgroundTime > 30 * 1000) {
             SessionIdsManager.createSessionId();
             reportEndEvent();
             reportStartEvent();
-        }else if(inBackgroundTime == 0){
+        } else if (inBackgroundTime == 0) {
             reportStartEvent();
-        }else {
+        } else {
             inBackgroundTime = 0l;
         }
     }
 
-    public void sendExitEvent(){
+    public void sendExitEvent() {
         reportEndEvent();
     }
 
-    private void reportStartEvent(){
+    private void reportStartEvent() {
         processStartTime = System.currentTimeMillis();
-        EventCommonParams eventCommonParams = new EventCommonParams("$AppStart",System.currentTimeMillis());
-        EventBean eventBean = new EventBean();
-        eventBean.setEventCommonParams(eventCommonParams);
-        ReportManager.getInstance().sendRealTimeEvent(eventBean);
-    }
-    private void reportEndEvent(){
-        long duration = System.currentTimeMillis() - processStartTime;
-        EventCommonParams eventCommonParams = new EventCommonParams("$AppEnd",System.currentTimeMillis(),duration/1000);
+        EventCommonParams eventCommonParams = new EventCommonParams("$AppStart", System.currentTimeMillis());
         EventBean eventBean = new EventBean();
         eventBean.setEventCommonParams(eventCommonParams);
         ReportManager.getInstance().sendRealTimeEvent(eventBean);
     }
 
-    public void reportInstallEvent(){
-        EventCommonParams eventCommonParams = new EventCommonParams("$AppInstall",System.currentTimeMillis());
+    private void reportEndEvent() {
+        long duration = System.currentTimeMillis() - processStartTime;
+        EventCommonParams eventCommonParams = new EventCommonParams("$AppEnd", System.currentTimeMillis(), duration / 1000);
+        EventBean eventBean = new EventBean();
+        eventBean.setEventCommonParams(eventCommonParams);
+        ReportManager.getInstance().sendRealTimeEvent(eventBean);
+    }
+
+    public void reportInstallEvent() {
+        EventCommonParams eventCommonParams = new EventCommonParams("$AppInstall", System.currentTimeMillis());
         EventBean eventBean = new EventBean();
         eventBean.setEventCommonParams(eventCommonParams);
         ReportManager.getInstance().sendRealTimeEvent(eventBean);
